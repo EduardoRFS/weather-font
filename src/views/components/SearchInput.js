b8a7350d53606b63912bfc7d3be26685e48c8b79
EduardoRFS <theeduardorfs@gmail.com>
2019-05-17 16:13:38 -0300

    small fix in searchInput css

--- src/views/components/SearchInput.js
+++ src/views/components/SearchInput.js
@@ -1,13 +1,14 @@
 import './SearchInput.css';
 import React from 'react';
 
-const SearchInput = ({ value, ...props }) => (
-  <div className="SearchInput" {...props}>
+const SearchInput = ({ locked, value, ...props }) => (
+  <div className={`SearchInput ${locked && 'SearchInput--locked'}`} {...props}>
     <label htmlFor="search-input" className="SearchInput_container">
       <input
         id="search-input"
         className="SearchInput__input"
         value={value}
+        disabled={locked}
         {...props}
       />
       {!value && <span className="SearchInput__label">Buscar por cidade</span>}
