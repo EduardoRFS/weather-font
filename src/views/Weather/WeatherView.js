18f5937deacaba98cfa210e939a429bd2a004cb1
EduardoRFS <theeduardorfs@gmail.com>
2019-05-22 10:58:58 -0300

    WeatherView show main

--- src/views/Weather/WeatherView.js
+++ src/views/Weather/WeatherView.js
@@ -1,10 +1,12 @@
 import './WeatherView.css';
 import React from 'react';
 import WeatherHeader from './components/WeatherHeader';
+import WeatherMain from './components/WeatherMain';
 
 const WeatherView = ({ weather }) => (
   <div className="WeatherView">
     <WeatherHeader weather={weather} />
+    <WeatherMain weather={weather} />
   </div>
 );
 
